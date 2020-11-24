fb2dda38d4b6296e3e856d6bbc936d403055d1ce
dgarcia4 <dgarcia4@oswego.edu>
2020-11-24 16:35:35 -0500

    Wrote the generated key into a text file

--- txt_encrypt.py
+++ txt_encrypt.py
@@ -8,13 +8,22 @@ import os
 actual_password = "csc332Crypto<3" #this is the string form of the password
 password = actual_password.encode() #converts to bytes
 
+#b is to know that its bits
+#pads the password with extra bits
 salt = b'qQqbHctmkCEdh_RFhvIy5Qg5yPSDeEFpNnyThdGgM3c='
+
+#all in one method
+
 kdf = PBKDF2HMAC(
-algorithm=hashes.SHA256(),
+algorithm = hashes.SHA256(),
 length=32,
 salt = salt,
 iterations = 100000,
 backend=default_backend()
 )
+
 key = base64.urlsafe_b64encode(kdf.derive(password)) #only uses kdf once
-print(key)
+#print(key)
+file = open('hashtest1.txt', 'wb')
+file.write(key)
+file.close()
