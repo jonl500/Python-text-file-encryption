02adf932473d9e50022844b691526329a6abfa00
jonl500 <jonl500@gmail.com>
2020-12-01 21:50:33 -0500

    update for encrypt

--- crypto2.py
+++ crypto2.py
@@ -1,5 +1,6 @@
 import hashlib
 import base64
+from base64 import b64encode, b64decode
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -8,44 +9,49 @@ from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
 
 password = input('enter password: ')
-password2 = password.encode()
 
 salt = b'qQqbHctmkCEdh_RFhvIy5Qg5yPSDeEFpNnyThdGgM3c='
 
 #all in one method
 
-kdf = PBKDF2HMAC(
-algorithm = hashes.SHA256(),
-length=32,
-salt = salt,
-iterations = 100000,
-backend=default_backend()
-)
+# kdf = PBKDF2HMAC(
+# algorithm = hashes.SHA256(),
+# length=32,
+# salt = salt,
+# iterations = 100000,
+# backend=default_backend()
+#)
 
 #encode password
-key = base64.urlsafe_b64encode(kdf.derive(password2))
+#key = base64.urlsafe_b64encode(kdf.derive(password2))
+private_key = hashlib.scrypt(
+        password.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
 
 with open('hashtest1.txt','rb') as file:
     hashed_password = file.read()
 
-#pattern matching
-if key == hashed_password:
-    print("Success!")
-    plain_text = input("plain text here:")
-else :
-   print('Fail :(')
 
 def encrypt(plain_text):
-     cipher_config = AES.new(private_key, AES.MODE_GCM)
+    cipher_config = AES.new(private_key, AES.MODE_GCM)
 
     # return a dictionary with the encrypted text
     cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plain_text, 'utf-8'))
     return {
-        'cipher_text': b64encode(cipher_text).decode('utf-8'),
-        'salt': b64encode(salt).decode('utf-8'),
-        'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
-        'tag': b64encode(tag).decode('utf-8')
+        'cipher_text': base64.b64encode(cipher_text).decode('utf-8'),
+        'salt': base64.b64encode(salt).decode('utf-8'),
+        'nonce': base64.b64encode(cipher_config.nonce).decode('utf-8'),
+        'tag': base64.b64encode(tag).decode('utf-8')
         }
-        file = open('encrypted_text.txt', 'wb')
-        file.write(cipher_text)
-        file.close()
+    file = open('encrypted_text.txt', 'wb')
+    file.write(cipher_text)
+    file.close()
+
+
+#pattern matching
+if private_key == hashed_password:
+    print("Success!")
+    plain_text = input("plain text here:")
+    encrypt(plain_text)
+else :
+   print('Fail :(')
+
